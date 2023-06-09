e5bd993081aac65280cdeb1fc010815079839ad7
GemmaTuron <gemma@ersilia.io>
2023-06-09 11:19:05 +0200

    gene ids

--- src/process_pharmgkb/gene.py
+++ src/process_pharmgkb/gene.py
@@ -26,6 +26,8 @@ def create_table():
     data_dict = {}
     for r in df.values:
         gid = c.stringify(r[0])
+        hgnc = c.stringify(r[2])
+        ensembl = c.stringify(r[3])
         gene = c.stringify(r[5])
         vip = c.stringify(r[8])
         if vip == "No":
@@ -48,16 +50,18 @@ def create_table():
             dosing_guideline = 1
         else:
             dosing_guideline = 0
-        data_dict[gid] = [gene, vip, variant_annotation, dosing_guideline]
+        data_dict[gid] = [gene, hgnc, ensembl, vip, variant_annotation, dosing_guideline]
     data = pd.DataFrame.from_dict(data_dict, orient="index")
     data.reset_index(inplace=True)
     data.rename(
         columns={
             "index": "gid",
             0: "gene",
-            1: "vip",
-            2: "variant_annotation",
-            3: "dosing_guideline",
+            1: "hgnc_id",
+            2: "ensembl_id",
+            3: "vip",
+            4: "variant_annotation",
+            5: "dosing_guideline",
         },
         inplace=True,
     )
