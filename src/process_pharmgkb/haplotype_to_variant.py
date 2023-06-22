a2354e7a55b8ca2f290fdbb5c0d6b34655033c5f
GemmaTuron <gemma@ersilia.io>
2023-06-22 15:57:59 +0200

    hid to vid complete

--- src/process_pharmgkb/haplotype_to_variant.py
+++ src/process_pharmgkb/haplotype_to_variant.py
@@ -97,18 +97,74 @@ def add_vars(df):
                            else row['NC'].strip() if pd.notnull(row['NC'])
                            else str(row["gene"] + " " + row['start']).strip() if pd.notnull(row['gene']) and pd.notnull(row['start'])
                            else None, axis=1)
+    df2["variant"] = df2["variant"].apply(lambda var: var.rstrip("*") if  pd.notnull(var) else var)
+    df2.loc[df2["variant"] == "NAT2 Δ859", "variant"] = "NAT2 859Del"
     vid_mapping = df.groupby('variant')['vid'].first().reset_index() #get unique vid-variant
     df2 = df2.merge(vid_mapping, on='variant', how='left')
     df2['vid'] = df2['vid'].fillna('')
     df2 = df2.replace('', None)
     return df2
 
+def deconv_doubles(df):
+    R = []
+    for r in df.values:
+        hid = r[0]
+        hap = r[1]
+        hap_num = r[2]
+        gid = r[3]
+        gene = r[4]
+        rsid = r[5]
+        start = r[6]
+        protein = r[7]
+        nc = r[8]
+        ng = r[9]
+        var = r[10]
+        vid = r[11]
+        if (vid == None) & ("HLA" not in gene):
+            if ";" in str(start):
+                st_split = [s.strip() for s in start.split(";")]
+                nc_split = [s.strip() for s in nc.split(";")]
+                if pd.notnull(protein):
+                    protein_split = [s.strip() for s in protein.split(";")]
+                    ng_split = [s.strip() for s in ng.split(";")]
+                    for i, s in enumerate(st_split):
+                        start = st_split[i]
+                        nc = nc_split[i]
+                        protein = protein_split[i]
+                        ng = ng_split[i]
+                        r_ = [hid, hap, hap_num, gid, gene, rsid, start, protein, nc, ng, var, vid]
+                        R += [r_]
+                else:
+                    for i, s in enumerate(st_split):
+                        start = st_split[i]
+                        nc = nc_split[i]
+                        r_ = [hid, hap, hap_num, gid, gene, rsid, start, protein, nc, ng, var, vid]
+                        R += [r_]
+            else:
+                r_ = [hid, hap, hap_num, gid, gene, rsid, start, protein, nc, ng, var, vid]
+                R += [r_]
+        else:
+            r_ = [hid, hap, hap_num, gid, gene, rsid, start, protein, nc, ng, var, vid]
+            R += [r_]
+    cols = []
+    cols = ["hid", "haplotype", "haplotype_number", "gid", "gene", "rsID", "start", "protein", "nc", "ng", "variant", "vid"]
+    data = pd.DataFrame(R, columns=cols)
+    data.drop(columns=["vid"], inplace=True)
+    df2 = pd.read_csv(os.path.join(processed_folder, "hid_to_vid.csv"))
+    vid_mapping = df2.groupby('variant')['vid'].first().reset_index() #get unique vid-variant
+    data = data.merge(vid_mapping, on='variant', how='left')
+    data['vid'] = data['vid'].fillna('')
+    data = data.replace('', None)
+    return data
+                        
+
 if __name__ == "__main__":
-    #df = pd.read_csv(os.path.join(processed_folder, "haplotype.csv"))
-    #print_hids(df)
-    #data = get_variants(df)
-    #data.to_csv(os.path.join(processed_folder, "hid_to_vid.csv"), index=False)
-    #check_hids()
+    df = pd.read_csv(os.path.join(processed_folder, "haplotype.csv"))
+    print_hids(df)
+    data = get_variants(df)
+    data.to_csv(os.path.join(processed_folder, "hid_to_vid.csv"), index=False)
+    check_hids()
     data = pd.read_csv(os.path.join(processed_folder, "hid_to_vid.csv"))
     data = add_vars(data)
-    data.to_csv(os.path.join(processed_folder, "hid_to_vid_.csv"), index=False)
+    data = deconv_doubles(data)
+    data.to_csv(os.path.join(processed_folder, "hid_vid_complete.csv"), index=False)
