49ecd3c2d57c241a2f68f2a0ecfc695c9b0bc149
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2023-09-04 20:11:21 +0200

    work in progress variant annotation

--- src/splitter.py
+++ src/splitter.py
@@ -1,1 +1,12 @@
 # for now, check the AI notebook
+
+import pandas as pd
+import numpy as np
+import random
+from rdkit import Chem
+from tqdm import tqdm
+from sklearn.metrics import roc_auc_score
+import sys
+from lol import LOL
+from rdkit import RDLogger 
+RDLogger.DisableLog('rdApp.*')
