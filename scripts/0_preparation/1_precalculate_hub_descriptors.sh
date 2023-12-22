9bcd2d069eeebc58017f8ed9c753bb8d9cbdccfe
GemmaTuron <gemma@ersilia.io>
2023-12-22 15:42:02 +0100

    automate desc normalisation

--- scripts/0_preparation/1_precalculate_hub_descriptors.sh
+++ scripts/0_preparation/1_precalculate_hub_descriptors.sh
@@ -1,14 +1,6 @@
 # Bash script to fetch and precalculate the descriptors for molecules
 # can be edited to incorporate more descriptors
 
-#ersilia -v fetch eos4u6p 
-ersilia serve eos4u6p
-ersilia -v run -i "$1/drug_molecules.csv" -o "$1/eos4u6p.h5"
-
-ersilia -v fetch eos2gw4
-ersilia serve eos2gw4
-ersilia -v run -i "$1/drug_molecules.csv" -o "$1/eos2gw4.h5"
-
-ersilia -v fetch eos8a4x
-ersilia serve eos8a4x
-ersilia -v run -i "$1/drug_molecules.csv" -o "$1/eos8a4x.h5"
+ersilia -v fetch $1
+ersilia serve $1
+ersilia -v run -i "$2/drug_molecules.csv" -o "$2/$1.h5"
--- scripts/0_preparation/2_normalize_chemical_h5_files.py
+++ scripts/0_preparation/2_normalize_chemical_h5_files.py
@@ -1,17 +1,23 @@
 import os
 import sys
+import json
 
 root = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(root, "..", "..", "src"))
 
 from utils import H5Normalizer
 
-normalizer = H5Normalizer(
-    os.path.join(root, "..", "..", "data", "chemical_descriptors", "eos8a4x.h5")
-)
-normalizer.run()
+available_descs = os.path.join(root, "..", "..", "data", "chemical_descriptors", "available_descs.json")
 
-normalizer = H5Normalizer(
-    os.path.join(root, "..", "..", "data", "chemical_descriptors", "eos78ao.h5")
-)
-normalizer.run()
+with open(available_descs, "r") as f:
+    data = json.load(f)
+
+descs = [eosid for eosid in data["chemical_descriptors"].keys() if data["chemical_descriptors"][eosid]["normalized"]==True]
+
+for eosid in descs:
+    try:
+        descs_path = os.path.join(root, "..", "..", "data", "chemical_descriptors", f"{eosid}.h5")
+        normalizer = H5Normalizer(descs_path)
+        normalizer.run()
+    except:
+        print(f"Descriptor {eosid} not calculated")
